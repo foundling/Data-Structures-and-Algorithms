315ea93b11e320d07078efca178634a85ba24e65
Alex Ramsdell <alexramsdell@gmail.com>
2016-07-17 22:15:14 -0700

    add alternate method to show duplicates

--- show_duplicates.py
+++ show_duplicates.py
@@ -13,3 +13,17 @@ def show_dup(seq):
 
     return dup_dict
 
+def show_dup_alt(seq):
+    dup_dict = {}
+    # for each unique key in seq
+    for m in set(seq):
+        # get all ms in sequence
+        ms = filter(seq, key=lambda x: x == m)
+        # assign first to key, that's guaranteed
+        # assign second to remaining
+        key, dups = ms[0], ms[1:]
+        dup_dict[key] = len(dups)
+
+    return dup_dict
+
+
